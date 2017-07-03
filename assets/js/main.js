dc3cca7d1e361a10afa4e6656361590f730e9df7
Artem Kaloev <kaloev.eltech@gmail.com>
2017-07-03 23:17:39 +0300

    New func: isEmpty(obj)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+//Пример функции и ее теста
 /*
  * @param {number} x Число для возведения в степень
  * @param {number} n Показатель степени, натуральное число
@@ -29,10 +30,11 @@ describe("pow", function() {
 });
 
 
+
+//Еще один пример функции и ее теста
 function sum(a, b) {
 	return a + b; 
 }
-
 
 describe("sum", function() {
 
@@ -42,6 +44,9 @@ describe("sum", function() {
 
 });
 
+
+
+// Функция работы с числами (приведение с помощью унарного плюса)
 // function getAndSumPrices() {
 // 	var doublePrice1 = +prompt("Введите первую цену (в виде десятичной дроби)", "");
 // 	var doublePrice2 = +prompt("Введите вторую цену (в виде десятичной дроби)", "");
@@ -51,9 +56,12 @@ describe("sum", function() {
 // 	alert(sum.toFixed(1));
 
 // }
-
 //getAndSumPrices();
 
+
+
+
+//Работа с объектом
 var user = {};
 
 user.name = "Вася";
@@ -64,5 +72,43 @@ delete user.name;
 
 
 
+//Перебор свойств объекта
+var menu = {
+  width: 300,
+  height: 200,
+  title: "Menu"
+};
+
+for (var key in menu) {
+  // этот код будет вызван для каждого свойства объекта
+  // ..и выведет имя свойства и его значение
+
+  //alert( "Ключ: " + key + " значение: " + menu[key] );
+  //!! menu[key] - использованы квадратные скобки для доступа к значению свойства.
+  //!! Если имя свойства хранится в переменной, то обратиться к свойству можно только так, а не через точку.
+}
+
+
+
+//Функция проверки объекта (пустой/непустой)
+function isEmpty(obj) {
+	for (var prop in obj) return false;
+	return true;
+}
+ 
+describe("isEmpty", function() {
+
+	var schedule = {};
+	it("Дан пустой объект ", function() {
+		assert.equal(isEmpty(schedule), true);
+	});
+
+	it("Дан НЕпустой объект ", function() {
+			schedule["8:30"] = "подъем";
+			assert.equal(isEmpty(schedule), false);
+		});
+	
+
+});
 
 
